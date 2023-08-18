913a3852efe5e54fc4f1b05cb14e1be3e4d7b05f
Naim Hasan <naimarafat7@gmail.com>
2023-08-18 00:40:56 -0700

    Make User Admin API

--- index.js
+++ index.js
@@ -27,12 +27,43 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
+        //User related APIS
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+
         //user sing in info data store API
+
         const userCollection = client.db("bistroMenuItemsDb").collection("users");
         app.post('/users', async (req, res) => {
             const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User Already exists' });
+            }
             const result = await userCollection.insertOne(user);
             res.send(result);
+
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
 
         })
 
